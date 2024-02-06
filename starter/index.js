c7784541115a103f57f8f37e0bdc323d64540e53
Billy Rowden <williampsr.rowden@gmail.com>
2024-02-06 16:54:57 +0000

    added input questions to question array

--- starter/index.js
+++ starter/index.js
@@ -5,7 +5,52 @@ const generateMarkdown = require("./utils/generateMarkdown");
 
 // array of questions for user
 const questions = [
-
+    {
+        type: "input",
+        name: "title",
+        message: "Please enter the name of your Project:",
+    },
+    {
+        type: "input",
+        name: "description",
+        message: "Please enter a description of the purpose and functionality of this Project:",
+    },
+    {
+        type: "input",
+        name: "installation",
+        message: "Please enter instructions for the user to install your application:",
+    },
+    {
+        type: "input",
+        name: "usage",
+        message: "Please explain how your application is used:",
+    },
+    {
+        type: "input",
+        name: "license",
+        message: "Please choose a license for your application:",
+        choice: ['MIT', 'Apache 2.0', 'BSL1.0', 'GPLv3.0', 'none']
+    },
+    {
+        type: "input",
+        name: "contributions",
+        message: "Please mention any contribution guidelines for your Project:",
+    },
+    {
+        type: "input",
+        name: "tests",
+        message: "Please enter any test intructions for your application:",
+    },
+    {
+        type: "input",
+        name: "githubuser",
+        message: "Please enter a link to your GitHub Profile:",
+    },
+    {
+        type: "input",
+        name: "email",
+        message: "Please enter your email address:",
+    },
 ];
 
 // function to write README file
